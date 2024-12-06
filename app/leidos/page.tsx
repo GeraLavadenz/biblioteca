133b448969f1740799cef71c3812a16f6fae0d5a
Geraldiene <riomahabramromamateo@gmail.com>
2024-12-06 16:55:04 -0400

    v1

---
+++ app/leidos/page.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Leidos = () => {
+  return (
+    <div>Leidos</div>
+  )
+}
+
+export default Leidos
---
+++ app/por_leer/page.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const PorLeer = () => {
+  return (
+    <div>PorLeer</div>
+  )
+}
+
+export default PorLeer
