d6ca862b036302677360ec17157fc9f896974b1f
Geraldiene <riomahabramromamateo@gmail.com>
2024-12-06 18:24:40 -0400

    v2 sidevar

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,8 @@
 import type { Metadata } from "next";
 import localFont from "next/font/local";
 import "./globals.css";
+import { SidebarProvider, SidebarTrigger } from "@/components/ui/sidebar";
+import { AppSidebar } from "@/components/AppSidebar";
 
 const geistSans = localFont({
   src: "./fonts/GeistVF.woff",
@@ -30,6 +32,13 @@ export default function RootLayout({
       >
         {children}
       </body>
+      <SidebarProvider>
+        <AppSidebar />
+        <main>
+          <SidebarTrigger />
+          {children}
+        </main>
+    </SidebarProvider>
     </html>
   );
 }
